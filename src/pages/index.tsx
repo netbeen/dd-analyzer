a7133f04b1ad10386d5449bf83ad0c6213044ec6
Yang Yang <yangyang.jeff@bytedance.com>
2022-04-30 19:23:00 +0800

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
 import styles from './index.less';
-import { Upload, message, Button } from 'antd';
+import { Upload, Popover, Button } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { useEffect, useState } from 'react';
 import { cardValues, PokerMatrics } from '@/component/PokerMatrics';
 import { WinPassLoseIndicator } from '@/component/WinPassLoseIndicator';
-
-const heroName = 'YY';
+import { convertTextToGameRecord, IGameRecord } from '@/service/gameRecord';
 
 export default function IndexPage() {
   const [rawText, setRawText] = useState<string>('');
   const [gameRecords, setGameRecords] = useState<any[]>([]);
 
-  const props = {
+  const uploadProps = {
     name: 'file',
     listType: 'txt',
     beforeUpload(file: any) {
@@ -25,16 +24,6 @@ export default function IndexPage() {
     },
   };
 
-  const extractHand = (line: string) => {
-    const extractHandsReg = / \[ (\w{2}) ]$/;
-    const regResult = line.match(extractHandsReg);
-    if (Array.isArray(regResult)) {
-      return regResult[1];
-    } else {
-      return '';
-    }
-  };
-
   useEffect(() => {
     const gameRecords = rawText.split(
       '-----------------------------------------------------\r\n\r\n',
@@ -43,38 +32,9 @@ export default function IndexPage() {
     const formattedGameRecords = gameRecords
       .slice(0, gameRecords.length - 1)
       .map((textRecord) => {
-        const lines = textRecord.split('\r\n');
-        const dealingIndex = lines.indexOf('Dealing...');
-        const summaryIndex = lines.indexOf('*** SUMMARY ***');
-        const heroResult = lines
-          .slice(summaryIndex + 1)
-          .find((line) => line.includes(heroName));
-        const hands = [
-          extractHand(lines[dealingIndex + 1]),
-          extractHand(lines[dealingIndex + 2]),
-        ];
-        const win = heroResult?.includes('net ');
-        const lost = heroResult?.includes('lost');
-        const pass = heroResult?.includes("didn't bet");
-        return {
-          textRecord,
-          id: lines[0].split(' - ')[0],
-          type: lines[0].split(' - ')[1],
-          description: lines[0].split(' - ')[2],
-          bigBlindChips: Number(
-            lines[0].split(' - ')[2].match(/^(\d*)\/(\d*) /)?.[2],
-          ),
-          timestamp: new Date(lines[0].split(' - ')[3]),
-          dealingIndex,
-          hands,
-          win,
-          lost,
-          pass,
-          winBigBlinds: 0,
-        };
+        return convertTextToGameRecord(textRecord);
       });
     setGameRecords(formattedGameRecords);
-    console.log('formattedGameRecords', formattedGameRecords);
     console.log(
       'formattedGameRecords',
       formattedGameRecords[formattedGameRecords.length - 1],
@@ -89,8 +49,8 @@ export default function IndexPage() {
       formattedGameRecords.filter((item) => item.pass).length,
     );
     console.log(
-      'Lost手数',
-      formattedGameRecords.filter((item) => item.lost).length,
+      'Lose手数',
+      formattedGameRecords.filter((item) => item.lose).length,
     );
     console.log(
       'Win率',
@@ -103,98 +63,72 @@ export default function IndexPage() {
         formattedGameRecords.length,
     );
     console.log(
-      'Lost率',
-      formattedGameRecords.filter((item) => item.lost).length /
+      'Lose率',
+      formattedGameRecords.filter((item) => item.lose).length /
         formattedGameRecords.length,
     );
     console.log(
       formattedGameRecords.filter(
-        (item) => !item.lost && !item.pass && !item.win,
+        (item) => !item.lose && !item.pass && !item.win,
       ),
     );
   }, [rawText]);
 
   return (
     <div>
-      <Upload {...props}>
-        <Button icon={<UploadOutlined />}>Click to Upload</Button>
-      </Upload>
+      {gameRecords.length === 0 && (
+        <Upload {...uploadProps}>
+          <Button icon={<UploadOutlined />}>Click to Upload</Button>
+        </Upload>
+      )}
       <div>
         胜/弃/负：
         <PokerMatrics
           cell={cardValues.map((rowCard, rowIndex) =>
             cardValues.map((columnCard, columnIndex) => {
+              let totalRecords: IGameRecord[] = [];
+              let winRecords: IGameRecord[] = [];
+              let passRecords: IGameRecord[] = [];
+              let loseRecords: IGameRecord[] = [];
               if (rowIndex < columnIndex) {
-                // s
-                const records = gameRecords.filter(
+                totalRecords = gameRecords.filter(
                   (gameRecord) =>
                     gameRecord.hands[0].startsWith(cardValues[rowIndex]) &&
                     gameRecord.hands[1].startsWith(cardValues[columnIndex]) &&
                     gameRecord.hands[0][1] === gameRecord.hands[1][1],
                 );
-                return (
-                  <WinPassLoseIndicator
-                    winPercentage={
-                      records.filter((record) => record.win).length /
-                      records.length
-                    }
-                    passPercentage={
-                      records.filter((record) => record.pass).length /
-                      records.length
-                    }
-                    losePercentage={
-                      records.filter((record) => record.lost).length /
-                      records.length
-                    }
-                  />
-                );
               } else if (rowIndex > columnIndex) {
-                const records = gameRecords.filter(
+                totalRecords = gameRecords.filter(
                   (gameRecord) =>
                     gameRecord.hands[0].startsWith(cardValues[rowIndex]) &&
                     gameRecord.hands[1].startsWith(cardValues[columnIndex]) &&
                     gameRecord.hands[0][1] !== gameRecord.hands[1][1],
                 );
-                return (
-                  <WinPassLoseIndicator
-                    winPercentage={
-                      records.filter((record) => record.win).length /
-                      records.length
-                    }
-                    passPercentage={
-                      records.filter((record) => record.pass).length /
-                      records.length
-                    }
-                    losePercentage={
-                      records.filter((record) => record.lost).length /
-                      records.length
-                    }
-                  />
-                );
               } else {
-                // pair
-                const records = gameRecords.filter(
+                totalRecords = gameRecords.filter(
                   (gameRecord) =>
                     gameRecord.hands[0].startsWith(cardValues[rowIndex]) &&
                     gameRecord.hands[1].startsWith(cardValues[columnIndex]),
                 );
-                return (
-                  <WinPassLoseIndicator
-                    winPercentage={
-                      records.filter((record) => record.win).length /
-                      records.length
-                    }
-                    passPercentage={
-                      records.filter((record) => record.pass).length /
-                      records.length
-                    }
-                    losePercentage={
-                      records.filter((record) => record.lost).length /
-                      records.length
-                    }
-                  />
-                );
               }
+              winRecords = totalRecords.filter((record) => record.win);
+              passRecords = totalRecords.filter((record) => record.pass);
+              loseRecords = totalRecords.filter((record) => record.lose);
+
+              return (
+                <Popover
+                  content={`胜:${winRecords.length} 弃:${passRecords.length} 负:${loseRecords.length}`}
+                  trigger="hover"
+                >
+                  <div>
+                    <WinPassLoseIndicator
+                      winPercentage={winRecords.length / totalRecords.length}
+                      passPercentage={passRecords.length / totalRecords.length}
+                      losePercentage={loseRecords.length / totalRecords.length}
+                    />
+                  </div>
+                </Popover>
+              );
             }),
           )}
         />
---
+++ src/service/gameRecord.ts
@@ -0,0 +1,60 @@
+export interface IGameRecord {
+  textRecord: string;
+  id: string;
+  type: string;
+  description: string;
+  bigBlindChips: number;
+  timestamp: Date;
+  hands: [string, string];
+  win: boolean;
+  lose: boolean;
+  pass: boolean;
+  winBigBlinds: number;
+}
+
+const extractHand: (line: string) => string = (line) => {
+  const extractHandsReg = / \[ (\w{2}) ]$/;
+  const regResult = line.match(extractHandsReg);
+  if (Array.isArray(regResult)) {
+    return regResult[1];
+  } else {
+    return '';
+  }
+};
+
+export const convertTextToGameRecord: (textRecord: string) => IGameRecord = (
+  textRecord,
+) => {
+  const lines = textRecord.split('\r\n');
+  const dealingIndex = lines.indexOf('Dealing...');
+  const summaryIndex = lines.indexOf('*** SUMMARY ***');
+  const heroName = textRecord.match(/Dealt to (\w*) \[/)?.[1];
+  if (!heroName) {
+    throw new Error();
+  }
+  const heroResult = lines
+    .slice(summaryIndex + 1)
+    .find((line) => line.includes(heroName));
+  const hands: [string, string] = [
+    extractHand(lines[dealingIndex + 1]),
+    extractHand(lines[dealingIndex + 2]),
+  ];
+  const win = heroResult?.includes('net ') ?? false;
+  const lose = heroResult?.includes('lost') ?? false;
+  const pass = heroResult?.includes("didn't bet") ?? false;
+  return {
+    textRecord,
+    id: lines[0].split(' - ')[0],
+    type: lines[0].split(' - ')[1],
+    description: lines[0].split(' - ')[2],
+    bigBlindChips: Number(
+      lines[0].split(' - ')[2].match(/^(\d*)\/(\d*) /)?.[2],
+    ),
+    timestamp: new Date(lines[0].split(' - ')[3]),
+    hands,
+    win,
+    lose,
+    pass,
+    winBigBlinds: 0,
+  };
+};
